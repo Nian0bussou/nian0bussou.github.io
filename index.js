3ae2c2da4570d2ef79d1eb1df9c71f28243ddde2
Nian0bussou <nianlegare@outlook.com>
2024-12-18 23:53:49 -0500

    Wed Dec 18 23:53:49 EST 2024

--- index.js
+++ index.js
@@ -39,52 +39,54 @@ document.addEventListener('DOMContentLoaded', () => {
         createBR(),
     ]);
     // Create collapsible sections
-    createCollapsible(body, 'ソーシャル', `<ul>
-      ${li_GetListElement(`Discord/ ashita_nian`)},
-      ${li_GetListElement(`Github/ ${a_GetAElement("https://github.com/Nian0Bussou", "Nian0Bussou")}`)}
-      ${li_GetListElement(`Twitter/ ${a_GetAElement("https://x.com/NianToshi", "NianToshi")}`)}
-      ${li_GetListElement(`Bluesky/ ${a_GetAElement("https://bsky.app/profile/ashita-nian.bsky.social", "ashita-nian")}`)}
-    </ul>
-    <sub><b><i> I have no other way of being contacted</i></b></sub>
-    `, 0);
-    createCollapsible(body, '読める言語', `<ul>
-      ${li_GetListElement("フランス語")}
-      ${li_GetListElement("英語")}
-      ${li_GetListElement("日本語")}
-    </ul>`, 1);
-    createCollapsible(body, 'プログラミング言語の経験順にランキング', `<ol>
-      ${nameIcon('Go        ', 'go')}
-      ${nameIcon('C#        ', 'c--4')}
-      ${nameIcon('HTML      ', 'html')}
-      ${nameIcon('Unity     ', 'unity')}
-      ${nameIcon('CSS       ', 'css')}
-      ${nameIcon('Rust      ', 'rust')}
-      ${nameIcon('F#        ', 'fsharp')}
-      ${nameIcon('Haskell   ', 'haskell')}
-      ${nameIcon('C++       ', 'c++')}
-      ${nameIcon('C         ', 'c')}
-      ${nameIcon('OCamL     ', 'ocaml')}
-      ${nameIcon('Python    ', 'python')}
-      ${nameIcon('Ecmascript', 'js')}
-    </ol>`, 2);
-    createCollapsible(body, '好きです', `<ul>
-      ${li_GetListElement('Arcane')}
-      ${li_GetListElement("Arknights' lore")}
-      ${li_GetListElement("Wis'adel")}
-      ${li_GetListElement('Overwatch 2')}
-    </ul>`, 3);
-    createCollapsible(body, '好きない', `<ul>
-      ${li_GetListElement('人')}
-      ${li_GetListElement('LoL')}
-    </ul>`, 4);
-    createCollapsible(body, '便利（べんり）連関（れんかん）', `<ul>
-      ${li_GetListElement(`${a_GetAElement("https://translate.google.com", "translate")}`)}
-      ${li_GetListElement(`${a_GetAElement("https://www.reddit.com", "jlailu")}`)}
-      ${li_GetListElement(`${a_GetAElement("https://jisho.org", "辞書")}`)}
-      ${li_GetListElement(`${a_GetAElement("https://sapling.ai/lang/japanese", "sapling")}`)}
-    </ul>`, 3);
+    appendList(body, [
+        createCollapsibleAndContent(body, 'ソーシャル', `<ul>
+        ${li_GetListElement(`Discord/ ashita_nian`)},
+        ${li_GetListElement(`Github/ ${a_GetAElement("https://github.com/Nian0Bussou", "Nian0Bussou")}`)}
+        ${li_GetListElement(`Twitter/ ${a_GetAElement("https://x.com/NianToshi", "NianToshi")}`)}
+        ${li_GetListElement(`Bluesky/ ${a_GetAElement("https://bsky.app/profile/ashita-nian.bsky.social", "ashita-nian")}`)}
+      </ul>
+      <sub><b><i> I have no other way of being contacted</i></b></sub>
+      `, 0),
+        createCollapsibleAndContent(body, '読める言語', `<ul>
+        ${li_GetListElement("フランス語")}
+        ${li_GetListElement("英語")}
+        ${li_GetListElement("日本語")}
+      </ul>`, 1),
+        createCollapsibleAndContent(body, 'プログラミング言語の経験順にランキング', `<ol>
+        ${nameIcon('Go        ', 'go')}
+        ${nameIcon('C#        ', 'c--4')}
+        ${nameIcon('HTML      ', 'html')}
+        ${nameIcon('Unity     ', 'unity')}
+        ${nameIcon('CSS       ', 'css')}
+        ${nameIcon('Rust      ', 'rust')}
+        ${nameIcon('F#        ', 'fsharp')}
+        ${nameIcon('Haskell   ', 'haskell')}
+        ${nameIcon('C++       ', 'c++')}
+        ${nameIcon('C         ', 'c')}
+        ${nameIcon('OCamL     ', 'ocaml')}
+        ${nameIcon('Python    ', 'python')}
+        ${nameIcon('Ecmascript', 'js')}
+      </ol>`, 2),
+        createCollapsibleAndContent(body, '好きです', `<ul>
+        ${li_GetListElement('Arcane')}
+        ${li_GetListElement("Arknights' lore")}
+        ${li_GetListElement("Wis'adel")}
+        ${li_GetListElement('Overwatch 2')}
+      </ul>`, 3),
+        createCollapsibleAndContent(body, '好きない', `<ul>
+        ${li_GetListElement('人')}
+        ${li_GetListElement('LoL')}
+      </ul>`, 4),
+        createCollapsibleAndContent(body, '便利（べんり）連関（れんかん）', `<ul>
+        ${li_GetListElement(`${a_GetAElement("https://translate.google.com", "translate")}`)}
+        ${li_GetListElement(`${a_GetAElement("https://www.reddit.com", "jlailu")}`)}
+        ${li_GetListElement(`${a_GetAElement("https://jisho.org", "辞書")}`)}
+        ${li_GetListElement(`${a_GetAElement("https://sapling.ai/lang/japanese", "sapling")}`)}
+      </ul>`, 3)
+    ]);
 });
-function createCollapsible(body, title, content, index) {
+function createCollapsibleAndContent(body, title, content, index) {
     const style = document.createElement('style');
     style.textContent = `
                         .content${index} {
@@ -106,11 +108,9 @@ function createCollapsible(body, title, content, index) {
     contentDiv.style.marginLeft = "2em";
     contentDiv.style.marginBottom = "1em";
     button.addEventListener('click', () => { contentDiv.classList.toggle('open'); });
-    appendList(body, [
-        button,
-        contentDiv,
-        createBR(),
-    ]);
+    const div = document.createElement('div');
+    appendList(div, [button, contentDiv, createBR()]);
+    return div;
 }
 function nameIcon(name, icon) {
     return li_GetListElement(`${name} <img class="icon" src="/svgs/${icon}.svg" alt=" icon" />`);
